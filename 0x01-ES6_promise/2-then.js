fcb95ffb94d155d6d24405cb36930e9145175032
== <==>
2023-12-04 16:54:11 +0000

    think fast: then catch

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,12 @@
 function handleResponseFromAPI(promise) {
   return promise
     .then((response) => {
-        console.log('Got a response from the API');
-        return { status: 200, body: 'success' };
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
     })
     .catch((error) => {
-        return new Error();
-    });
+      return new Error();
+  });
 }
 
 export default handleResponseFromAPI
