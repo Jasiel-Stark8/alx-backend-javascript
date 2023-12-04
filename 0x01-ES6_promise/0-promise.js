10ae0653f1dde85176e43393ede2a5afb5aba59f
== <==>
2023-12-04 10:26:42 +0000

    rebase

--- 0x01-ES6_promise/0-promise.js
+++
@@ -1,8 +0,0 @@
-function getFullResponseFromAPI(apiEndpoint) {
-  return new Promise((resolve, reject) => {
-    if (apiEndpoint) resolve({ status: 200, body: 'Success' });
-    reject(Error('Failed to fetch API'));
-  });
-}
-// Export the function for external use
-export default getFullResponseFromAPI;
--- 0x01-ES6_promise/README.md
+++
@@ -1,1 +0,0 @@
-promise to update later
