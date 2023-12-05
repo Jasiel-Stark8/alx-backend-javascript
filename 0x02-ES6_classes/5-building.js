3eba163cd6f64e136b40db757eeebd0d6582ba47
== <==>
2023-12-05 21:06:29 +0000

    ecvacuate the building oooo

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,1 +1,17 @@
- 
+class Building {
+    constructor(sqft) {
+        if (typeof sqft !== 'number') throw new Error();
+        this._sqft = sqft;
+        this.evacuationWarningMessage();
+    }
+
+    get sqft() {
+        return this._sqft;
+    }
+
+    evacuationWarningMessage() {
+        if (new.target === Building) throw new Error("Building is an abstract class and cannot be instantiated directly");
+    }
+}
+
+export default Building;
