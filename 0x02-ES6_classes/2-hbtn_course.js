2cd83980625e2b5524d7e154471a9a232f7ba323
== <==>
2023-12-05 15:02:22 +0000

    Get Set Construct Validate Holberton

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,32 +1,59 @@
 class HolbertonCourse {
   constructor(name, length, students){
+    this.validateName(name);
+    this.validateLength(length);
+    this.validateStudents(students);
+
     this._name = name;
     this._length = length;
     this._students = students;
-  }
-  set name(name) {
-    this._name = name;
-  }
-
-  set length(length) {
-    this._length = length;
-  }
-
-  set students(students) {
-    this._students = student;
   }
 
   get name() {
     return this._name;
   }
 
+  set name(newName) {
+    this.validateName(newName);
+    this._name = newName;
+  }
+
   get length() {
     return this._length;
+  }
+
+  set length(newLength) {
+    this.validateLength(newLength);
+    this._length = newLength;
   }
 
   get students() {
     return this._students;
   }
+
+  set students(newStudent) {
+    this.validateStudents(newStudent)
+    this._students = newStudent;
+  }
+
+  validateName(name) {
+    if (typeof name !== 'string') {
+        throw new Error('Name must be a string');
+    }
+  }
+
+  validateLength(length) {
+    if (typeof length !== 'number') {
+        throw new Error('Length must be a number');
+    }
+  }
+
+  validateStudents(students) {
+    if (typeof students !== 'string') {
+        throw new Error('Student must be a string');
+    }
+  }
+
 }
 
 export default HolbertonCourse
