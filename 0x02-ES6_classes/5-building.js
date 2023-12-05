c08edfe5c8e99516e67816abf2ba426f5992c186
== <==>
2023-12-05 21:20:50 +0000

    evacuate the building ooo

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,20 @@
-class Building {
-    constructor(sqft) {
-        if (typeof sqft !== 'number') throw new Error();
-        this._sqft = sqft;
-        this.evacuationWarningMessage();
-    }
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') throw new Error();
+    this._sqft = sqft;
+    this.evacuationWarningMessage();
+  }
 
-    get sqft() {
-        return this._sqft;
-    }
+  get sqft() {
+    return this._sqft;
+  }
 
-    evacuationWarningMessage() {
-        if (new.target === Building) throw new Error("Building is an abstract class and cannot be instantiated directly");
-    }
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+    // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    if (this.constructor.name !== 'Building') throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
 }
-
-export default Building;
