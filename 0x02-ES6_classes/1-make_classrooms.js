587ced5ac4ab8254b7948c9d48a0ad15d3111ebe
== <==>
2023-12-05 13:33:34 +0000

    invoke new class room

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms(maxStudentsSize) {
-    return [
-        new ClassRoom(19),
-        new ClassRoom(20),
-        new ClassRoom(34)
-    ]
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34)
+  ]
 }
