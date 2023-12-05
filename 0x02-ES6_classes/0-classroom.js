2d6e03262475e863745b3737e96e50f6e626c96d
== <==>
2023-12-05 13:11:25 +0000

    Classssssssss

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,7 @@
-const getUsers  = async () => {
-    const url = 'https://twitter.com/';
-    const resolve = await axios.get(url);
-    console.log(resolve);
-    return await resolve;
+class ClassRoom {
+    constructor(maxStudentsSize) {
+        this._maxStudentsSize = maxStudentsSize; 
+    }
 }
+
+export default ClassRoom
