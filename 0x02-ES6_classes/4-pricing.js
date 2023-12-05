acc7fe2e024a357580b783589e13fc4a5dfba8cf
== <==>
2023-12-05 16:14:19 +0000

    full abundance

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,37 +1,37 @@
 import Currency from './3-currency.js';
 
 class Pricing {
-  constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
-  }
-
-  get amount() {
-    return this._amount;
-  }
-
-  set amount(newAmount) {
-    this._amount = newAmount;
-  }
-
-  get currency() {
-    return this._currency;
-  }
-
-  set currency(newCurrency) {
-    if (!(newCurrency instanceof currency)) {
-        throw new TypeError('Expected a currency instance')
+    constructor(amount, currency) {
+        this._amount = amount;
+        this._currency = currency;
     }
-    this._currency = newCurrency;
-  }
 
-  displayFullPrice() {
-    return `${this._amount} ${this._currency.name} ${this.currency.code}`;
-  }
+    get amount() {
+        return this._amount;
+    }
 
-  static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
-  }
+    set amount(newAmount) {
+        this._amount = newAmount;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+
+    set currency(newCurrency) {
+        if (!(newCurrency instanceof Currency)) {
+            throw new TypeError('Expected a Currency instance');
+        }
+        this._currency = newCurrency;
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    }
+
+    static convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
+    }
 }
 
 export default Pricing;
