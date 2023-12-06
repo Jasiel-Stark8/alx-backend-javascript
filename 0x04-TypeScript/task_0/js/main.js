038c6d7f40c04444022ff43c7333ffd0f8ca5e27
== <==>
2023-12-06 17:09:22 +0000

    TypeScript Student Interface

--- 0x04-TypeScript/task_0/js/main.js
+++ 0x04-TypeScript/task_0/js/main.js
@@ -1,18 +1,18 @@
 var student1 = {
-    firstName: '',
-    lastName: '',
-    age: 1,
-    location: ''
+    firstName: 'Alice',
+    lastName: 'Smith',
+    age: 24,
+    location: 'California'
 };
 var student2 = {
-    firstName: '',
-    lastName: '',
-    age: 1,
-    location: ''
+    firstName: 'Bob',
+    lastName: 'Johnson',
+    age: 30,
+    location: 'New York'
 };
 var studentsList = [student1, student2];
 var tableRows = studentsList.map(function (student) {
-    return "\n    <tr>\n        <td>".concat(student.firstName, "</td>\n        <td>").concat(student.lastName, "</td>\n        <td>").concat(student.age, "</td>\n        <td>").concat(student.location, "</td>\n    </tr>");
+    return "\n  <tr>\n    <td>".concat(student.firstName, "</td>\n    <td>").concat(student.lastName, "</td>\n    <td>").concat(student.age, "</td>\n    <td>").concat(student.location, "</td>\n  </tr>");
 }).join('');
-var table = "\n<table>\n  <thead>\n    <tr>\n      <th>Column 1 Heading</th>\n      <th>Column 2 Heading</th>\n      <th>Column 3 Heading</th>\n    </tr>\n  </thead>\n  <tbody>\n    <tr>\n    ".concat(tableRows, "\n    </tr>\n  </tbody>\n</table>");
+var table = "\n<table>\n  <thead>\n    <tr>\n      <th>firstName</th>\n      <th>lastName</th>\n      <th>age</th>\n      <th>location</th>\n    </tr>\n  </thead>\n  <tbody>\n    ".concat(tableRows, "\n  </tbody>\n</table>");
 document.body.innerHTML += table;
--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,43 +1,44 @@
 interface Student {
-    firstName: string;
-    lastName: string;
-    age: number;
-    location: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
 }
 
 const student1: Student = {
-    firstName: 'Alice',
-    lastName: 'Smith',
-    age: 24,
-    location: 'California'
+  firstName: 'Alice',
+  lastName: 'Smith',
+  age: 24,
+  location: 'California'
 };
 
 const student2: Student = {
-    firstName: 'Bob',
-    lastName: 'Johnson',
-    age: 30,
-    location: 'New York'
+  firstName: 'Bob',
+  lastName: 'Johnson',
+  age: 30,
+  location: 'New York'
 };
 
 const studentsList: Student[] = [student1, student2];
 
 let tableRows = studentsList.map(student => {
-    return `
-    <tr>
-        <td>${student.firstName}</td>
-        <td>${student.lastName}</td>
-        <td>${student.age}</td>
-        <td>${student.location}</td>
-    </tr>`;
+  return `
+  <tr>
+    <td>${student.firstName}</td>
+    <td>${student.lastName}</td>
+    <td>${student.age}</td>
+    <td>${student.location}</td>
+  </tr>`;
 }).join('');
 
 const table = `
 <table>
   <thead>
     <tr>
-      <th>Column 1 Heading</th>
-      <th>Column 2 Heading</th>
-      <th>Column 3 Heading</th>
+      <th>firstName</th>
+      <th>lastName</th>
+      <th>age</th>
+      <th>location</th>
     </tr>
   </thead>
   <tbody>
